5b0326c78c0e4f467518c296ac74a83482e640d1
Davis Sylvester <davis@sylvesterllc.com>
2023-05-29 01:39:24 -0500

    Adding routable property for using use Custom Route Override

--- src/resources/helpers/createRoutes.ts
+++ src/resources/helpers/createRoutes.ts
@@ -5,39 +5,43 @@ import { TsgLambdaProp } from "../../config/types";
 
 export class Routes {
 
-    public static Resources: Resource[] = [];  
-    private static routeMap = new Map();       
+    public static Resources: Resource[] = [];
+    private static routeMap = new Map();
 
-    public static createResource(        
-        prop: TsgLambdaProp, 
-        api: IRestApi, 
-        lambdaNode: NodejsFunction, 
+    public static createResource(
+        prop: TsgLambdaProp,
+        api: IRestApi,
+        lambdaNode: NodejsFunction,
         authorizer?: TokenAuthorizer) {
 
         const routeMap: Map<string, Resource> = new Map();
 
+        let activeRoutePath = "";
+        let activeResource: Resource | undefined = undefined;
+
         //  Only attach lambda to an Api Gateway if a route exist
         if (prop.apiGateway?.route) {
 
-        //  Note:  this now uses the bundle version as the first segment in the path.
-        let activeRoutePath = (prop.apiGateway.useRouteOverride) ? "/" : `v${(prop.apiGateway.version) ? prop.apiGateway.version : 1 }`;
-        let activeResource: Resource = Routes.routeMap.get(activeRoutePath) ||  api.root.addResource(activeRoutePath);
-        Routes.routeMap.set(activeRoutePath, activeResource);
+            if (!prop.apiGateway.useRouteOverride) {
+                //  Note:  this now uses the bundle version as the first segment in the path.
+                activeRoutePath = `v${(prop.apiGateway.version) ? prop.apiGateway.version : 1}`;
+                activeResource = Routes.routeMap.get(activeRoutePath) || api.root.addResource(activeRoutePath);
+                Routes.routeMap.set(activeRoutePath, activeResource);
+            }
+            //  Now we go through our route segments creating the rest of the path.
+            const pathSegments = prop.apiGateway?.route.split("/").filter(x => (x));
+            for (let i = 0; i < pathSegments.length; i++) {
+                activeRoutePath = `${activeRoutePath}/${pathSegments[i]}`;
+                let secondaryResource = Routes.routeMap.get(activeRoutePath) || activeResource!.addResource(pathSegments[i]);
+                Routes.routeMap.set(activeRoutePath, secondaryResource);
+                activeResource = secondaryResource;
+            }
 
-        //  Now we go through our route segments creating the rest of the path.
-        const pathSegments = prop.apiGateway?.route.split("/").filter(x => (x));
-        for (let i = 0; i < pathSegments.length; i++) {
-            activeRoutePath = `${activeRoutePath}/${pathSegments[i]}`;
-            let secondaryResource = Routes.routeMap.get(activeRoutePath) || activeResource!.addResource(pathSegments[i]);                
-            Routes.routeMap.set(activeRoutePath, secondaryResource);                                  
-            activeResource = secondaryResource;                
+            //  Finally, we attach our function to the last resource
+            activeResource!.addMethod(prop.apiGateway.method || 'GET',
+                new LambdaIntegration(lambdaNode, { proxy: true, }),
+                prop.apiGateway.secure ? { authorizer } : undefined);
         }
-        
-        //  Finally, we attach our function to the last resource
-        activeResource.addMethod(prop.apiGateway.method || 'GET',
-            new LambdaIntegration(lambdaNode, { proxy: true, }), 
-            prop.apiGateway.secure ? { authorizer } : undefined);    
     }
-}
 
 }
