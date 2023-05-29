7863e06f047e87c71a8286c6d7b7015743629cc0
Davis Sylvester <davis@sylvesterllc.com>
2023-05-29 01:31:04 -0500

    Adding routable property for using Versioning

--- src/config/types/TsgLambdaRoutable.ts
+++ src/config/types/TsgLambdaRoutable.ts
@@ -3,5 +3,5 @@ export interface TsgLambdaRoutable {
     secure?: boolean;
     method?: "get" | "post" | "delete" | "put" | "patch";
     version?: number;
-    useVersioning?: boolean;
+    useRouteOverride?: boolean;
 }
