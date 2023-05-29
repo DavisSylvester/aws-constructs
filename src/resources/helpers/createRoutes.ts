fce8743e8bebc6d6db1ce2642dd03720413b4eed
Davis Sylvester <davis@sylvesterllc.com>
2023-05-29 13:33:45 -0500

    Updating route

--- src/resources/helpers/createRoutes.ts
+++ src/resources/helpers/createRoutes.ts
@@ -24,20 +24,21 @@ export class Routes {
             
             console.log('ROute: ', prop.apiGateway.route);
 
+            if (!prop.apiGateway.useRouteOverride)  {
             //  First we create the root resource if it doesn't exist.
                 //  Note:  this now uses the bundle version as the first segment in the path.
-                activeRoutePath = (prop.apiGateway.useRouteOverride)? "/" : `v${(prop.apiGateway.version) ? prop.apiGateway.version : 1}`;
+                activeRoutePath =  `v${(prop.apiGateway.version) ? prop.apiGateway.version : 1}`;
                 activeResource = Routes.routeMap.get(activeRoutePath) || api.root.addResource(activeRoutePath);
                 Routes.routeMap.set(activeRoutePath, activeResource);
-
-                console.log('Routes.routeMap: ', Routes.routeMap);
-                console.log('activeResource: ', activeResource);
+            }
+    
             
             //  Now we go through our route segments creating the rest of the path.
             const pathSegments = prop.apiGateway?.route.split("/").filter(x => (x));
+
             for (let i = 0; i < pathSegments.length; i++) {
                 activeRoutePath = `${activeRoutePath}/${pathSegments[i]}`;
-                let secondaryResource = Routes.routeMap.get(activeRoutePath) || activeResource!.addResource(pathSegments[i]);
+                let secondaryResource = Routes.routeMap.get(activeRoutePath) || activeResource?.addResource(pathSegments[i]) || api.root.addResource(activeRoutePath);
                 Routes.routeMap.set(activeRoutePath, secondaryResource);
                 activeResource = secondaryResource;
             }
