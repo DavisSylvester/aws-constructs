be05712510f6104142c0ba20b43d4728a6472fed
Davis Sylvester <davis@sylvesterllc.com>
2023-05-29 02:01:36 -0500

    Updating route

--- src/resources/helpers/createRoutes.ts
+++ src/resources/helpers/createRoutes.ts
@@ -21,13 +21,12 @@ export class Routes {
 
         //  Only attach lambda to an Api Gateway if a route exist
         if (prop.apiGateway?.route) {
-
-            if (!prop.apiGateway.useRouteOverride) {
+            
                 //  Note:  this now uses the bundle version as the first segment in the path.
-                activeRoutePath = `v${(prop.apiGateway.version) ? prop.apiGateway.version : 1}`;
+                activeRoutePath = (prop.apiGateway.useRouteOverride)? "/" : `v${(prop.apiGateway.version) ? prop.apiGateway.version : 1}`;
                 activeResource = Routes.routeMap.get(activeRoutePath) || api.root.addResource(activeRoutePath);
                 Routes.routeMap.set(activeRoutePath, activeResource);
-            }
+            
             //  Now we go through our route segments creating the rest of the path.
             const pathSegments = prop.apiGateway?.route.split("/").filter(x => (x));
             for (let i = 0; i < pathSegments.length; i++) {
