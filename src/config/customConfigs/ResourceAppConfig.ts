ed46d81fabc9343b3cefdf4e85ea2beb9ed0a646
Davis Sylvester <davis.sylvester-iii@siemens.com>
2023-10-11 13:34:34 -0500

    removing prefix from index name

--- src/config/customConfigs/ResourceAppConfig.ts
+++ src/config/customConfigs/ResourceAppConfig.ts
@@ -2,6 +2,7 @@ import {
     TsgDynamoDbProp,
     TsgLambdaProp,
 } from "../types";
+import { TsgEC2Prop } from "../types/TsgEc2Prop";
 import { TsgLambdaLayerProp } from "../types/TsgLambdaLayerProp";
 
 export interface ResourceAppConfig {    
@@ -9,4 +10,6 @@ export interface ResourceAppConfig {
     LAMBDA_LAYERS?: TsgLambdaLayerProp[];
     AUTHORIZER?: TsgLambdaProp;
     LAMBDA: TsgLambdaProp[];
+    EC2: TsgEC2Prop[];
+    
 }
