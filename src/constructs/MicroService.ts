5b007f3df8cb0f28bc2e3b83c6fe72ec51d1437d
Davis Sylvester <davis.sylvester-iii@siemens.com>
2023-12-09 11:36:59 -0600

    Updates

--- src/constructs/MicroService.ts
+++ src/constructs/MicroService.ts
@@ -10,12 +10,18 @@ import { Tags } from "aws-cdk-lib";
 import { LayerVersion } from "aws-cdk-lib/aws-lambda";
 import { CreateApiAndAttachLambdas } from "../resources/gateway/CreateApiAndAttachLambdas";
 import { createSeedDatabaseCustomResource } from "../resources/customResource/createSeedDatabaseCustomResource";
+import { IRestApi } from "aws-cdk-lib/aws-apigateway";
+import { ISecret } from "aws-cdk-lib/aws-secretsmanager";
 export class MicroService extends Construct {
 
     protected readonly requireDynamoTables: boolean;
     protected requireSeedDatabase: boolean = false;
     protected readonly hasLambdaLayers: boolean = false;  
     protected appConfig: AppConfig;
+    private readonly api: IRestApi;
+    private readonly dynamoTables: Table[] | undefined;
+    private readonly lambdaLayers: LayerVersion[] | undefined;
+    private readonly secretManager: ISecret | null = null;
 
     constructor(scope: Construct, id: string, props: MicroserviceProps) {
         super(scope, id);
@@ -31,20 +37,26 @@ export class MicroService extends Construct {
         this.hasLambdaLayers = (props.RESOURCES.LAMBDA_LAYERS && 
             props.RESOURCES.LAMBDA_LAYERS.length > 0) ? true : false;
 
-        this.onInit(scope);
+        const results = this.onInit(scope);
+
+        this.api = results.restApi!;
+        this.dynamoTables = results.dynamoTables!;
+        this.lambdaLayers = results.lambdaLayers!;
+        this.secretManager = results.secretManager!;
 
         this.createTag(scope)
     }
 
     private onInit(scope: Construct) {
 
+        let secretManager: ISecret | null = null;
         let tables: Table[] | undefined = undefined;
         let commonLayers: LayerVersion[] | undefined = undefined;
 
         if (process.env.SECRET_MANAGER_ARN) {
             // throw new Error(`You must provide the ARN for the your Configuration Secret 
             //     Manager`);      
-             const secretMgr = getSecretManager(scope, this.appConfig, process.env.SECRET_MANAGER_ARN);            
+             secretManager = getSecretManager(scope, this.appConfig, process.env.SECRET_MANAGER_ARN);            
         }
 
         if (this.hasLambdaLayers) {
@@ -68,6 +80,14 @@ export class MicroService extends Construct {
           
         // CREATE API GATEWAY AND LAMBDA HERE 
         const apiGateway = new CreateApiAndAttachLambdas(scope, this.appConfig, gateway[0], layers,tables);
+
+        return {
+            restApi: (gateway?.length > 0) ? gateway[0] : null,
+            lambdaLayers: layers || null,
+            dynamoTables: tables || null,
+            secretManager: secretManager,
+
+        };
        
     }
 
